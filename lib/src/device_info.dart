58cf72351c0cd5fbc56a6b04fffb720ec141b6b5
Rishabh Dev Narayan <rishabh.narayan@quokkalabs.com>
2025-01-17 16:44:51 +0530

    [FIX]: issue when recoding of logs on server.

--- lib/src/device_info.dart
+++ lib/src/device_info.dart
@@ -4,22 +4,23 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class DeviceInfo {
-  /// Private constructor to prevent instantiation.
-  DeviceInfo._privateConstructor();
+  static final DeviceInfo _instance = DeviceInfo._internal();
+  DeviceInfo._internal();
+  static DeviceInfo get instance => _instance;
 
   /// Static variables to store device information
-  static String deviceOS = "";
-  static String appName = "";
-  static String appVersion = "";
-  static String appEnv = "";
-  static String apiToken = "";
-  static String? userId;
-  static String? userName;
-  static String? deviceDetail;
-  static String? deviceID;
+  String deviceOS = "";
+  String appName = "";
+  String appVersion = "";
+  String appEnv = "";
+  String apiToken = "";
+  String? userId;
+  String? userName;
+  String? deviceDetail;
+  String? deviceID;
 
   /// Method used to set device information
-  static void setDeviceInfo() async {
+  Future<void> setDeviceInfo() async {
     final info = await PackageInfo.fromPlatform();
     var deviceInfo = DeviceInfoPlugin();
     if (Platform.isIOS) {
